7200451e409b81aa68cb42e2b8775bb61ead34b1
Aaron Berns <anberns@gmail.com>
2019-01-11 16:05:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,15 +17,5 @@ class Song
   def artist_name
     @artist
   end
-  describe '#artist_name' do
-    it 'knows the name of its artist' do
-      drake = Artist.new('Drake')
-      song.artist = drake
-      expect(song.artist_name).to eq('Drake')
-    end
-
-    it 'returns nil if the song does not have an artist' do
-      expect(song.artist_name).to eq nil
-    end
-  end
+  
 end
