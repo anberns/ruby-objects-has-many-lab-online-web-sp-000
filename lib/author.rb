a2a3763792e1c3b3c7a0ebc436454330304e5331
Aaron Berns <anberns@gmail.com>
2019-01-11 16:27:22 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,6 +12,14 @@ def Author
     @posts << post
   end
   
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self 
+    posts << post
+  end
   
-    
+  def self.post_count
+    Post.all.size 
+  end
+ 
 end
