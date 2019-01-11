923ff81acef233484f4095914c96b3453783ba67
Aaron Berns <anberns@gmail.com>
2019-01-11 16:05:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,10 +7,25 @@ class Song
   def initialize(name)
     @name = name 
     @@all << self
+    @artist = nil 
   end 
   
   def self.all 
     @@all 
   end
   
+  def artist_name
+    @artist
+  end
+  describe '#artist_name' do
+    it 'knows the name of its artist' do
+      drake = Artist.new('Drake')
+      song.artist = drake
+      expect(song.artist_name).to eq('Drake')
+    end
+
+    it 'returns nil if the song does not have an artist' do
+      expect(song.artist_name).to eq nil
+    end
+  end
 end
